05930b203a28e575106c8a4f32512dae12fedcb4
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2021-07-04 17:18:03 +0900

    코드 폴더구조 변경
    공유 코드 영역과 작업 영역별 코드 영역으로 코드 구분
    폴더명
    SharedCode : 공유 코드 영역. 환경 설정 변수와 같은 공통 정의 영역에 대한 정의가 포함된 코드 영역임.
    HJH : 입력 코드 작업을 위해 구분해둔 코드 영역.

---
+++ Assets/02.Script/SharedCode/Environment/EnvVR.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Environment
+{
+	/// <summary>
+	/// VR 인스턴스의 환경설정 변수 집합
+	/// </summary>
+	[System.Serializable]
+	public class EnvVR
+	{
+		
+	}
+}
