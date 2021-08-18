8350b518a9818dc75529781d1544c52d29a81027
Duruphil <josephine172@gmail.com>
2021-08-18 16:53:18 +0900

    laser prefab

---
+++ Assets/02.Script/Doi Test/LaserPointer.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class LaserPointer : MonoBehaviour
+{
+    public SteamVR_Input_Sources handType;
+    public SteamVR_Behaviour_Pose controllerPose;
+    public SteamVR_Action_Boolean teleportAction;
+
+    public GameObject laserPrefab;
+    public GameObject laser;
+    private Transform laserTransform;
+    private Vector3 hitPoint;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        laser = Instantiate(laserPrefab);
+        laserTransform = laser.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (teleportAction.GetState(handType))
+        {
+            RaycastHit hit;
+
+            if(Physics.Raycast(controllerPose.transform.position, transform.forward, out hit, 100))
+            {
+                hitPoint = hit.point;
+
+            }
+            else
+            {
+                laser.SetActive(false);
+            }
+        }
+
+       
+    }
+
+    private void ShowLaser(RaycastHit hit)
+    {
+        laser.SetActive(true);
+        laser.transform.position = Vector3.Lerp(controllerPose.transform.position, hitPoint, 0.5f);
+        laserTransform.LookAt(hitPoint);
+        laserTransform.localScale = new Vector3(laserTransform.localScale.x, laserTransform.localScale.y, hit.distance);
+    }
+}
