378e40e457c42d521fd92b4cccd0cbed030fe080
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2021-09-22 00:33:33 +0900

    Audio library import

---
+++ Assets/02.Script/HJH/Demo/AudioSwitcher/AudioSwitcher.cs
@@ -0,0 +1,27 @@
+//using AudioSwitcher.AudioApi.CoreAudio;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSwitcher
+{
+    //CoreAudioController controller;
+    //public AudioSwitcher.AudioApi.CoreAudio.CoreAudioController controller;
+
+    AudioBehaviour ab;
+
+    AudioSettings aus;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //AudioSettings.GetConfiguration().
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
