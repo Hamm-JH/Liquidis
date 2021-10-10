5e00a60e01cb30e43fe252464ca58fe6aa3e4953
DoiDoi <44909499+DoiDoiSound@users.noreply.github.com>
2021-10-10 22:04:25 +0900

    guid ani update

--- Assets/AniManager_Guide.cs
+++ Assets/AniManager_Guide.cs
@@ -7,6 +7,13 @@ public class AniManager_Guide : MonoBehaviour
 {
     public Animator guideAni;
     public Animator camera_rig;
+
+    bool cameraAniStart = false;
+    float currentTime = 0f;
+    float targetTime = 3f;
+    float zTarget = 62f;
+
+
     // Start is called before the first frame update
     //void Awake()
     //{
@@ -18,6 +25,26 @@ public class AniManager_Guide : MonoBehaviour
     {
         guideAni.SetTrigger("start");
         camera_rig.SetTrigger("SceneStart");
-    }    
-   
+    }
+
+    private void Update()
+    {
+        if (!cameraAniStart)
+        {
+            if(camera_rig.gameObject.transform.position.z >= zTarget)
+            {
+                currentTime += Time.deltaTime;
+
+                if(currentTime > targetTime)
+                {
+                    camera_rig.SetTrigger("SceneEnd");
+                    cameraAniStart = true;
+                }
+            }
+            else
+            {
+                currentTime = 0f;
+            }
+        }
+    }
 }
