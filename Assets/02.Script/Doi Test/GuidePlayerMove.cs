a139826c7827c65b2a47c8132f93a7f3b7fb0426
DoiDoi <44909499+DoiDoiSound@users.noreply.github.com>
2021-10-11 22:31:29 +0900

    guide update

--- Assets/02.Script/Doi Test/GuidePlayerMove.cs
+++ Assets/02.Script/Doi Test/GuidePlayerMove.cs
@@ -1,22 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GuidePlayerMove : MonoBehaviour
 {
 
     public float speed = 3f;
-    
+    public Animator camera_rig_ani;
+
+    bool done = false;
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+
+        if (!done)
         {
-            transform.Translate(new Vector3(0f, 0f, 1f) * Time.deltaTime * speed);
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(new Vector3(0f, 0f, 1f) * Time.deltaTime * speed);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(new Vector3(0f, 0f, -1f) * Time.deltaTime * speed);
+            }
         }
-        if (Input.GetKey(KeyCode.S))
+       
+
+        if(transform.position.z > 62f)
         {
-            transform.Translate(new Vector3(0f, 0f, -1f) * Time.deltaTime * speed);
+            if (!done)
+            {
+                MoveAniStart();
+                done = true;
+                return;
+
+            }
         }
+    }
+
+    void MoveAniStart()
+    {
+        camera_rig_ani.SetTrigger("SceneEnd");
+        StartCoroutine(LoadSelectScene());
+    }
+
+    IEnumerator LoadSelectScene()
+    {
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene("03_Play_Select");
     }
 }
