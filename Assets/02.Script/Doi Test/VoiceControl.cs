1d8d762d60e466897f62f41d0666a641acd0e3d7
DoiDoi <44909499+DoiDoiSound@users.noreply.github.com>
2021-10-23 23:31:47 +0900

    voice recorder

--- Assets/02.Script/Doi Test/VoiceControl.cs
+++ Assets/02.Script/Doi Test/VoiceControl.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Chat;
 using Photon.Pun;
+using Photon.Voice;
+using Photon.Voice.Unity;
+using Photon.Voice.PUN;
+
+
+
 
 public class VoiceControl : MonoBehaviour
 {
@@ -10,18 +16,24 @@ public class VoiceControl : MonoBehaviour
     public GameObject playerPref;
     public AudioClip playerClip;
     public AudioSource playerSource;
+   
+    PhotonVoiceNetwork photonVoiceNetwork;
 
     void Start()
     {
         //PhotonNetwork.Instantiate("player", Vector3.zero, Quaternion.Euler(Vector3.zero));
         //GenPlayerSpeaker();
+        photonVoiceNetwork = GetComponent<PhotonVoiceNetwork>();
     }
 
     public void GenPlayerSpeaker()
     {
         GameObject player;
         player = PhotonNetwork.Instantiate("player", Vector3.zero, Quaternion.Euler(Vector3.zero));
-        playerClip = player.GetComponentInChildren<AudioSource>().clip;
+        photonVoiceNetwork.ConnectAndJoinRoom();
+        photonVoiceNetwork.InitRecorder(photonVoiceNetwork.GetComponent<Recorder>());
+
+           playerClip = player.GetComponentInChildren<AudioSource>().clip;
         playerSource.clip = playerClip;
     }
 
