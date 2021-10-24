c1509b900cd7daf882d325ea8e85fa953703245e
DoiDoi <44909499+DoiDoiSound@users.noreply.github.com>
2021-10-24 21:26:08 +0900

    timescale push

---
+++ Assets/DuruLab/02.Scripts/TimeScale.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class TimeScale : MonoBehaviour
+{
+    // Toggles the time scale between 1 and 0.7
+    // whenever the user hits the Fire1 button.
+
+    private float fixedDeltaTime;
+
+    void Awake()
+    {
+        // Make a copy of the fixedDeltaTime, it defaults to 0.02f, but it can be changed in the editor
+        this.fixedDeltaTime = Time.fixedDeltaTime;
+    }
+
+    void Update()
+    {
+                Time.timeScale = 10f;
+         
+            // Adjust fixed delta time according to timescale
+            // The fixed delta time will now be 0.02 real-time seconds per frame
+            Time.fixedDeltaTime = this.fixedDeltaTime * Time.timeScale;
+        }
+    }
+}
