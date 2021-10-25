bae73d06566062ae14c0c903944d68ecfbfc6d4a
DoiDoi <44909499+DoiDoiSound@users.noreply.github.com>
2021-10-25 17:34:46 +0900

    meeting music sequence update

---
+++ Assets/02.Script/Doi Test/MeetingSequenceMusicQue.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeetingSequenceMusicQue : MonoBehaviour
+{
+
+    public AudioSource meetingAudioSource;
+    public AudioSource meetingAudioSource_sub;
+
+    public AudioClip shakebox_meeting;
+    public AudioClip face_wakeup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+   public void ShakeBoxPlay()
+    {
+        meetingAudioSource.clip = shakebox_meeting;
+        meetingAudioSource.Play();
+    }
+
+    public void FaceWakeUpPlay()
+    {
+        meetingAudioSource_sub.clip = face_wakeup;
+        meetingAudioSource_sub.Play();
+    }
+}
